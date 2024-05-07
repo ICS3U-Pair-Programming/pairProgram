5c1bb8dabbbdaa85f6d7b10e81e40736bc620304
kenjle-ICS <kenji.le@student.tdsb.on.ca>
2024-05-07 13:39:31 -0400

    i put in food questions

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Create an instance of the GuessNumber class/method
+        //Class Name ObejectName = NEW keyword Class Name
+        GuessNumber guessObject = new GuessNumber();
+        //call the object.method(); from our class
+        guessObject.guess();
     }
 }
